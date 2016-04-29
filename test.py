75a263045b24ab97966632106b8ebeedf0e21a30
Curtis Sand <curtis.sand@emc.com>
2016-04-29 15:15:55 -0600

    audio: sinusoid waveg generation changing frequency over time

--- test.py
+++ test.py
@@ -22,15 +22,22 @@ clip_length = 1  # seconds
 #
 # wavedata = numpy.random.randn(framerate * clip_length, channels)
 
-# Sine Wave Normalized between amplitude -1 and 1
-# The stereo tracks are duplicated to simulate mono
-frequency = 440  # Hz (Concert Pitch A4)
+# Sine Wave Generation
+# Data Normalized between -1 and 1
+# Sinusoid Wave Amplitude = math.sin(frame/((framerate/frequency)/math.pi))
+frequency = 440.0  # Hz (Concert Pitch A4)
 framecount = framerate * clip_length
 wavedata = numpy.ndarray((framecount, channels))
 for frame in range(framecount):
-    value = math.sin(frame/((framerate/frequency)/math.pi))
-    wavedata[frame, 0] = value
-    wavedata[frame, 1] = value
+    # Left track is 1 constant frequency
+    wavedata[frame, 0] = math.sin(frame/((framerate/frequency)/math.pi))
+
+    # Right track linearly progresses from 1 octave below to 1 octave above the
+    # chosen frequency
+    start_freq = frequency / 2
+    end_freq = frequency * 2
+    current_freq = start_freq + frame * (end_freq/framecount)
+    wavedata[frame, 1] = math.sin(frame/((framerate/current_freq)/math.pi))
 
 output_file = Sndfile(fname, 'w', Format('wav'), channels, framerate)
 output_file.write_frames(wavedata)
