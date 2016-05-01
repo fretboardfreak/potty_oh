ac697354475223e608dc56856e7f50392fcaa1e3
Curtis Sand <curtis@curtissand.com>
2016-05-01 13:19:47 -0600

    wav_file.py: add with statement support to WavFile

--- wav_file.py
+++ wav_file.py
@@ -30,3 +30,9 @@ class WavFile(object):
 
     def close(self):
         self._fhandle.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
