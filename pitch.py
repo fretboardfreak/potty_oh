08d52c20e9ab671d9b60c3f771c65dc97e411154
Curtis Sand <curtis@curtissand.com>
2016-05-03 11:01:48 -0600

    pitch.py: more work on calculating musical pitches
    Added classes to handle:
    - musical temperament
    - musical intervals
    - musical keys

--- pitch.py
+++ pitch.py
@@ -16,81 +16,147 @@
 """
 pitch.py: tools for calculating pitch frequencies.
 
-Ideas and theories to explore:
-
-- qualities (8 total) vs intervals (12 total)
-
-#. pythagorean diatonic tuning: [9/8, 5/4, 4/3, 3/2, 5/3, 15/8, 2]
-#. just intonation tuning: [9/8, 81/64, 4/3, 3/2, 27/16, 243/128, 2]
-#. meantone (quarter-comma) tempered
-#. mean (third-comma) tempered
-#. equal tempered (12fth root) [2^(x/12), ...]
-#. piano key representation (1-80)
+:TODO: piano key representation (1-80)
 """
 
-import string
+import common
 
 
-class PitchError(Exception):
-    pass
+class Interval(object):
+    """A mapping of interval types to one of the twelve semitones in a key."""
+    _intervals = {
+        'unison': 0, 'minor_second': 1, 'major_second': 2, 'minor_third': 3,
+        'major_third': 4, 'fourth': 5, 'augmented_fourth': 6, 'fifth': 7,
+        'minor_sixth': 8, 'major_sixth': 9, 'minor_seventh': 10,
+        'major_seventh': 11, 'octave': 12}
+
+    # Should alias 'seventh' be a minor seventh instead? flat 7 is often the
+    # intended meaning of "seventh". Or is it just the Minor Domininant 7th
+    # chord that use a flat 7?
+    _aliases = {
+        'second': 2, 'third': 4, 'diminished_fifth': 6,
+        'sixth': 9, 'seventh': 11}
+
+    # name -> semitones
+    _property_map = _intervals.copy()
+    # alias -> semitones
+    _property_map.update(_aliases)
+
+    # semitones -> name
+    _semitone_map = dict((value, key)
+                         for key, value in _intervals.items())
+
+    @classmethod
+    def name(cls, interval):
+        """Get the string name of an interval."""
+        return cls._semitone_map[interval]
+
+    @classmethod
+    def max(cls):
+        """Max number of intervals or semitones per octave."""
+        return len(cls._intervals.items()) - 1
+
+    @classmethod
+    def min(cls):
+        """For completeness."""
+        return 0
+
+
+class TemperamentType(list):
+    """A list of interval ratios to represent musical tuning temperament. """
+    def __init__(self, multipliers):
+        if len(multipliers) != Interval.max() + 1:
+            raise TypeError('%s multipliers are required, %s received.' %
+                            (Interval.max(), len(multipliers)))
+        super(TemperamentType, self).__init__(multipliers)
+
+    @classmethod
+    def set_interval_property(cls, name):
+        """Set an interval property on the temprement list."""
+
+        def method(cls):
+            return cls[getattr(Interval, name)]
+
+        setattr(cls, name, property(method))
+
+
+# add attributes to the Interval and TemperamentType classes
+for key, value in Interval._property_map.items():
+    setattr(Interval, key, value)
+    TemperamentType.set_interval_property(key)
+for semitone in range(13):
+    key = 'semitone%s' % semitone
+    setattr(Interval, key, semitone)
+    TemperamentType.set_interval_property(key)
+# clean up after myself
+del(key)
+del(value)
+del(semitone)
+
+
+class Temperament(object):
+    """A collection of some tuning temperaments used throughout history.
+
+    Other Temperaments to add:
+
+    - equal tempered/12th root tempered (freq=root*pow(2, (semitones/12)))
+    - quarter-comma mean tone tempered
+    - third-comma mean tone tempered
+    """
+    pythagorean = TemperamentType(
+        [1.0, 256.0/243.0, 9.0/8.0, 32.0/27.0, 81.0/64.0,
+         4.0/3.0, 729.0/512.0, 3.0/2.0, 128.0/81.0, 27.0/16.0,
+         16.0/9.0, 243.0/128.0, 2.0])
+
+    just = TemperamentType(
+        [1.0, 25.0/24.0, 9.0/8.0, 6.0/5.0, 5.0/4.0,
+         4.0/3.0, 45.0/32.0, 3.0/2.0, 8.0/5.0, 5.0/3.0,
+         9.0/5.0, 15.0/8.0, 2.0])
+
+    @classmethod
+    def iter(cls, *args, **kwarsg):
+        """Iterate over the set of temperaments."""
+        return iter([cls.pythagorean, cls.just])
 
 
 class ReferenceFrequencies(object):
+    """A collection of reference frequencies """
     scientific_middle_c = 256
 
     concert_a4 = 440
     middle_c = 261.626
 
 
-class TemperamentError(PitchError):
-    pass
+class Key(object):
+    """An object for calculating the note frequencies of musical keys."""
 
-
-class Temperament(list):
-    def __init__(self, multipliers):
-        if len(multipliers) != 7:
-            raise TemperamentError('Need 7 multipliers.')
-        super(Temperament, self).__init__(multipliers)
-
-
-PythagoreanTemperament = Temperament([9.0/8.0, 5.0/4.0, 4.0/3.0,
-                                      3.0/2.0, 5.0/3.0, 15.0/8.0, 2.0])
-JustTemperament = Temperament([9.0/8.0, 5.0/4.0, 4.0/3.0, 3.0/2.0,
-                               5.0/3.0, 15.0/8.0, 2.0])
-
-
-class Diatonic(object):
-
-    max_quality = 8  # next octave
-    min_quality = 1  # unison
-
-    def __init__(self, root_frequency=440.0, root_name="A",
-                 temperament=PythagoreanTemperament):
+    def __init__(self, root_frequency=ReferenceFrequencies.middle_c,
+                 root_name="A",
+                 temperament=Temperament.pythagorean):
         self.root_frequency = float(root_frequency)
         self.root_name = root_name
         self.temperament = temperament
 
-    def quality(self, quality, octave=4):
-        if isinstance(quality, int):
-            octave_root = self.root_frequency * pow(self.temperament[-1],
-                                                    (octave - 4))
-            print("octave root: %s" % octave_root)
-            index = (quality - 1) % 7
-            print('temperament index: %s' % index)
-            result = octave_root * self.temperament[index]
-            print('note freq: %s' % result)
-            return result
-        else:
-            raise NotImplemented('Letter based note qualities coming soon.')
-
+    def semitone(self, semitone, octave=0):
+        """Get the frequency of a given semitone relative to the root."""
+        common.dprint('semitone %s, octave %s :' % (semitone, octave))
+        index = semitone % (Interval.max() - 1)
+        common.dprint('  interval: %s' % (Interval.name(index)))
+        common.dprint('  root ratio: %s' % self.temperament[index])
+        octave += semitone // (Interval.max() - 1)
+        octave_root = (self.root_frequency *
+                       (self.temperament.octave * (octave + 1)))
+        common.dprint("  octave root: %s" % octave_root)
+        result = octave_root * self.temperament[index]
+        common.dprint('  note freq: %s' % result)
+        return result
 
 
 if __name__ == "__main__":
-    import common
     import waveform
     import wav_file
 
-    DOC="A demonstration of various musical notes and tunings."
+    DOC = "A demonstration of various musical notes and tunings."
 
     def main():
         parser = common.get_cmd_line_parser(description=DOC)
@@ -100,20 +166,23 @@ if __name__ == "__main__":
                                                'Length per pitch clip.')
         common.ParserArguments.plot(parser)
         common.ParserArguments.set_defaults(parser, filename='signal.wav',
-                                            length=1.0)
+                                            length=0.75)
         args = parser.parse_args()
 
-        sg = waveform.Generator(length=args.length, verbose=args.debug)
+        sg = waveform.Generator(length=args.length / 2.0, verbose=args.debug)
 
         if args.plot:
             raise NotImplemented()
         else:
-            notes = pitch.Diatonic()
+            pyth = Key()
+            just = Key(temperament=Temperament.just)
             with wav_file.WavFile(args.filename, 1, sg.framerate) as fout:
-                for quality in range(pitch.Diatonic.min_quality,
-                                     pitch.Diatonic.max_quality):
-                    fout.write(sg.sin_constant(notes.quality(quality)))
-                fout.write(sg.sin_constant(notes.quality(1, 5)))
+                for tone in range(Interval.max()):
+                    fout.write(sg.sin_constant(pyth.semitone(tone)))
+                    fout.write(sg.sin_constant(just.semitone(tone)))
+                sg.length = args.length
+                fout.write(sg.sin_constant(pyth.semitone(Interval.octave)))
+                fout.write(sg.sin_constant(just.semitone(Interval.octave)))
 
         return 0
 
