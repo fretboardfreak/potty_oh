2c55ac49e258b5f7782d724fa1c3687e1d1604ca
Curtis Sand <curtis@curtissand.com>
2016-05-04 08:30:13 -0600

    test.sh: add a test script to simplify/shorten the test cycle

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2016 Curtis Sand
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+# test.sh : Simplify the testing cycle.
+
+DEFAULT_FILE="signal.wav"
+
+PLAY="vlc -I ncurses --one-instance"
+
+die () {
+    echo $2
+    exit 1
+}
+
+[ $# -lt 1 ] && die 1 "Nothing to test. Need an argument."
+
+rm -f ${DEFAULT_FILE} && ./$@ --debug && ${PLAY} ${DEFAULT_FILE} && \
+    rm -f ${DEFAULT_FILE}
