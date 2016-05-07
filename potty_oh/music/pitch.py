bc1a4d08a8b191e441d91a90e60565c3bfdfa343
Curtis Sand <curtis@curtissand.com>
2016-05-07 13:58:42 -0600

    added music subpackage for basic music concepts

--- potty_oh/music/pitch.py
+++ potty_oh/music/pitch.py
@@ -19,7 +19,11 @@ pitch.py: tools for calculating pitch frequencies.
 :TODO: piano key representation (1-80)
 """
 
-import common
+from potty_oh.common import dprint
+from potty_oh.common import get_cmd_line_parser
+from potty_oh.common import ParserArguments
+from potty_oh.common import call_main
+
 from temperament import Temperament
 from interval import Interval
 
@@ -44,26 +48,26 @@ class Key(object):
 
     def interval(self, semitone, octave=0):
         """Get the frequency of a given interval relative to the root."""
-        common.dprint('semitone %s, octave %s :' % (semitone, octave))
+        dprint('semitone %s, octave %s :' % (semitone, octave))
         index = semitone % Interval.max()
-        common.dprint('  interval: %s' % (Interval.name(index)))
+        dprint('  interval: %s' % (Interval.name(index)))
         octave += semitone // Interval.max()
-        common.dprint('  octave: %s' % octave)
+        dprint('  octave: %s' % octave)
 
         # octaves are always 'root * 2' so use
         # 'root * pow(2, octave)' for new root.
         octave_root = (self.root_frequency *
                        (pow(self.temperament.octave, octave)))
-        common.dprint("  octave root: %s" % octave_root)
-        common.dprint('  interval ratio: %s' % self.temperament[index])
+        dprint("  octave root: %s" % octave_root)
+        dprint('  interval ratio: %s' % self.temperament[index])
         result = octave_root * self.temperament[index]
-        common.dprint('  note freq: %s' % result)
+        dprint('  note freq: %s' % result)
         return result
 
 
 if __name__ == "__main__":
-    import waveform
-    import wav_file
+    from potty_oh.waveform import Generator
+    from potty_oh.wav_file import wav_file_context
 
     DOC = ("A demonstration of various musical notes and tunings. The 12 "
            "semitones of a musical scale are played in each temperament. "
@@ -71,25 +75,25 @@ if __name__ == "__main__":
            "frequency of each note.")
 
     def main():
-        parser = common.get_cmd_line_parser(description=DOC)
-        common.ParserArguments.filename(parser)
-        common.ParserArguments.length(parser)
-        common.ParserArguments.modify_argument(parser, 'length', 'help',
+        parser = get_cmd_line_parser(description=DOC)
+        ParserArguments.filename(parser)
+        ParserArguments.length(parser)
+        ParserArguments.modify_argument(parser, 'length', 'help',
                                                'Length per pitch clip.')
-        common.ParserArguments.plot(parser)
-        common.ParserArguments.set_defaults(parser, length=0.75)
+        ParserArguments.plot(parser)
+        ParserArguments.set_defaults(parser, length=0.75)
         args = parser.parse_args()
 
-        sg = waveform.Generator(length=args.length / 2.0, verbose=args.debug)
+        sg = Generator(length=args.length / 2.0, verbose=args.debug)
 
         if args.plot:
             raise NotImplemented()
         else:
-            with wav_file.wav_file_context(args.filename) as fout:
+            with wav_file_context(args.filename) as fout:
                 key = Key()
                 for tone in range(Interval.max() + 1):
                     fout.write_frames(sg.sin_constant(key.interval(tone)))
 
         return 0
 
-    common.call_main(main)
+    call_main(main)
