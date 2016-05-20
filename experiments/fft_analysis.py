cdebffd24025bd1bf8f4675893e6c5bab2d04444
Curtis Sand <curtis.sand@emc.com>
2016-05-20 13:28:04 -0600

    fft_analysis.py: cleaned up some of the calculations
    Also added an FFT Analysis example that isn't as ideal os the first two.

--- experiments/fft_analysis.py
+++ experiments/fft_analysis.py
@@ -16,28 +16,35 @@
 """An experiment to help figure out how to use FFT analysis calculations."""
 
 from math import sqrt
-from numpy import round
-from scipy import fft
+from numpy import round, average
+from scipy import fftpack
 
 from potty_oh.common import get_cmd_line_parser
 from potty_oh.common import call_main
 from potty_oh.signal_generator import Generator
 
 
-def analyze_waveform(waveform):
+def analyze_whole_waveform(waveform):
+    """Determine the relative frequency powers present in the waveform.
+
+    1. take FFT of the whole waveform.
+    2. calculate power coefficients from FFT output
+    3. map the frequencies present in the waveform to their power values
+
+    The last example shows that, in some situations the FFT doesn't give a
+    clear indication of exactly one single frequency. Sometimes the FFT can
+    only give a reasonable estimate.
+    """
+    frequency_coefficients = fftpack.fft(waveform.frames)
     power_coefficients = [sqrt(pow(x.real, 2) + pow(x.imag, 2))
-                          for x in fft(waveform.frames)]
+                          for x in frequency_coefficients]
     bins = int(len(power_coefficients)/2)
     power_domain = round(power_coefficients[:bins])
-
-    niq_freq = waveform.framerate / 2.0  # max representable frequency
-    precision = niq_freq / bins  # step in Hz per FFT bin
-    print('{} samples at a framerate of {} samp/sec:\n  max_freq {}, '
-          'precision {}'.format(len(waveform.frames), waveform.framerate,
-                                niq_freq, precision))
-
-    powers = {(waveform.framerate * (index / niq_freq) * precision): strength
-              for index, strength in enumerate(power_domain)
+    frequency_domain = frequency_coefficients[:bins]
+    frequencies = fftpack.fftfreq(len(frequency_domain), 1.0 / waveform.framerate)
+    powers = {freq: coef
+              for freq, strength, coef in zip(frequencies, power_domain,
+                                              frequency_domain)
               if strength != 0.0}
     return powers
 
@@ -49,17 +56,23 @@ def main():
     sig_gen = Generator(verbose=args.debug)
 
     print('1 second waveform at 1000Hz')
-    frequency_powers = analyze_waveform(sig_gen.sin_constant(1000))
+    frequency_powers = analyze_whole_waveform(
+        sig_gen.sin_constant(1000))
     print("Frequency Powers found by FFT: ", frequency_powers)
 
     print('\n0.5 second waveform at 880Hz')
-    frequency_powers = analyze_waveform(sig_gen.sin_constant(880, length=0.5))
+    frequency_powers = analyze_whole_waveform(
+        sig_gen.sin_constant(880, length=0.5))
     print("Frequency Powers found by FFT: ", frequency_powers)
 
-    print('\n0.25 second waveform at 2000Hz')
-    frequency_powers = analyze_waveform(
-        sig_gen.sin_constant(2000, length=0.25))
-    print("Frequency Powers found by FFT: ", frequency_powers)
+    print('\n0.75 second waveform at 1234Hz')
+    frequency_powers = analyze_whole_waveform(
+        sig_gen.sin_constant(1234, length=0.75))
+    tmp = list(frequency_powers.items())
+    tmp.sort(key=lambda x: x[1], reverse=True)
+    print('{} present non-zero frequencies in signal. The most '
+          'powerfull frequencies are:\n{}'.format(
+              len(frequency_powers), '\n'.join([str(t) for t in tmp[:10]])))
 
     return 0
 
